b537d395cd9386ddfbf5087cda13e23465bb00fa
AyoTecca <kaliyevyesken@gmail.com>
2025-07-03 13:12:34 +0500

    Added new endpoint for checking tickets

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
 from flask import render_template
 from app import app
+from .utils import check_tickets  # Add this import
 
 @app.route("/")
 def home():
     return "Ticket Tracker Bot is running!"
+
+# Add new endpoint
+@app.route("/check-tickets")
+def trigger_check():
+    check_tickets()
+    return "Ticket check initiated!"
